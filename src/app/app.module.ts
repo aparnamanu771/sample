9c368d0c570abc40239df6ce8c0f3128d7554dca
manucrosspay <manu.kp@crosspaymt.com>
2023-02-06 22:01:08 +0530

    sampless

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { BankComponent } from './bank/bank.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    BankComponent,
+   
   ],
   imports: [
     BrowserModule
